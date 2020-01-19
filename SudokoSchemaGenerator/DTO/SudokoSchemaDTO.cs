cc8f3ce1fd95413d47d84b5bfea04104d08e6c70
Andrea Carratta <andrea.carratta.dev@outlook.it>
2020-01-19 10:28:45 +0100

    Generate Schema

---
+++ SudokoSchemaGenerator/DTO/SudokoSchemaDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokoSchemaGenerator.DTO
+{
+    public class SudokoSchemaDTO
+    {
+
+        public List<string> Solution { get; set; } = new List<string>();
+
+    }
+}
---
+++ SudokoSchemaGenerator/Logic/RowsLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SudokoSchemaGenerator.Logic
+{
+    public class RowsLogic
+    {
+
+        private const string NUMBERS = "123456789";
+
+        public string GenerateNew()
+        {
+            string result = Shuffle(NUMBERS);
+
+            return result;
+        }
+
+        private string Shuffle(string text)
+        {
+            Random num = new Random();
+
+            var chars = text.ToCharArray()
+                        .OrderBy(s => num.NextDouble())
+                        .ToArray();
+
+            return new string(chars);
+
+        }
+
+
+
+    }
+}
